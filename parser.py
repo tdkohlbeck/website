7f999b752e1829c1c31534978c69344bdf97157d
tdkohlbeck <tdkohlbeck+git@gmail.com>
2014-07-20 02:59:32 -0400

    work from 7/19/14

--- parser.py
+++ parser.py
@@ -1,17 +1,38 @@
-from mod_python import apache, util
+def parse(req, string):
+	word = ''
+	divDepth = 1
 
-def parse(req, a):
-  b = '<div>'
-  for c in a:
-    if c == '{':
-      b += '<div>'
-      c = ''
-    elif c == '}':
-      b += '</div>'
-      c = ''
-    elif c == '\n':
-      b += '<br/>'
-      c = '' 
-    b += c
-  b += '</div>'
-  return b
+	b = '<div>'
+	for char in string:
+		if char == '{':
+			b += '<div>'
+			divDepth += 1
+		elif char == '}':
+			b += '</div>'
+			divDepth -= 1
+		elif char == '\n':
+			b += '<br>'
+		else:
+			b += char
+	b += '</div>'
+
+class CodeBlock(object):
+	def __init__(self):
+		self.x = None
+		self.y = None
+		self.origin = None
+		self.children = None
+
+#	for c in a:
+#		if c == (' ' or '\n'):
+#			if word == '{':
+#				b += 'yay'
+#			elif word == '}':
+#				b += '</div>'
+#			else:
+#				b += word + c 
+#			word = ''
+#		else:
+#			word += c
+
+	return b
