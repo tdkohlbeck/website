bc99e6fb237681dd1133913f9f565f33e05f6478
tdkohlbeck <tdkohlbeck+git@gmail.com>
2014-08-23 20:58:22 -0700

    working on getting terminal text to appear in sidebar

--- codetographer.js
+++ codetographer.js
@@ -1,11 +1,23 @@
+// main
 window.onload = function() {
   var button = document.getElementById("submit_button");
   var terminal = document.getElementById("terminal");
 
   button.onclick = function() {
-    terminal.className = "animate";
-    var sidebar = document.createElement("div");
-    sidebar.id = "sidebar";
-    document.body.appendChild(sidebar);
+    terminal.className = "animate_pop_away";
+    //wait duration of pop_away then create and load sidebar
+    setTimeout(function(){ sidebarCreateAndLoad() }, 1200);
   };
+}
+
+// create and load sidebar
+function sidebarCreateAndLoad() {
+  var sidebar = document.createElement("div");
+  var terminalWindow = document.getElementById("window");
+  var userInput = document.createTextNode(terminalWindow.value);
+
+  sidebar.id = "sidebar";
+  sidebar.appendChild(userInput);
+  document.body.appendChild(sidebar);
+  sidebar.className = "code_text animate_show_sidebar";
 }
