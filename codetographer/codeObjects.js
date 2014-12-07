8bb52f12407ec1335d4f4a2cfb0d1c8feb86844e
tdkohlbeck <tdkohlbeck+git@gmail.com>
2014-12-07 07:54:22 +0000

    website reorganization and first commit for homepage!

---
+++ codetographer/codeObjects.js
@@ -0,0 +1,29 @@
+var general = {
+  "delim": [
+    " ", "\n",
+    "(", ")",
+    "[", "]",
+    "{", "}",
+    ";", "+"
+  ]
+}
+
+var C = {
+  "variable": [
+    "signed", "unsigned",
+    "short" , "long",
+    "const" , "char",
+    "int"   , "float",
+    "double", "bool",
+    "struct", "union"
+  ],
+
+  "logic": [
+    "if"    , "else",
+    "elseif", "switch",
+    "case"  , "default",
+    "do"    , "while",
+    "for"   , "goto",
+    "return"
+  ]
+}
